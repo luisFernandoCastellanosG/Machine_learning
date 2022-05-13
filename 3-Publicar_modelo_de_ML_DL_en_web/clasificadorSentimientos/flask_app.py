117add8b7a2da6607ea8d927ee5df337111fe0a6
luisFernandoCastellanosG <luis.castellanosg@usantoto.edu.co>
2022-05-12 20:25:15 -0500

    upload 2022
    upload 2022

--- 3-Publicar_modelo_de_ML_DL_en_web/clasificadorSentimientos/flask_app.py
+++ 3-Publicar_modelo_de_ML_DL_en_web/clasificadorSentimientos/flask_app.py
@@ -2,22 +2,30 @@ from flask import Flask, render_template, request
 from wtforms import Form, TextAreaField, validators
 
 app = Flask(__name__)
-
-class HelloForm(Form):
-    sayhello = TextAreaField('',[validators.DataRequired()])
+#clase que permite crear campos en formulario
+class evaluarForm(Form):
+    evaluarText = TextAreaField('',[validators.DataRequired(),
+                                    validators.length(min=15)])
 
 @app.route('/')
 def index():
-    form = HelloForm(request.form)
-    return render_template('second_app.html', form=form)
+    form = evaluarForm(request.form)
+    return render_template('index.html', form=form)
 
-@app.route('/hello', methods=['POST'])
-def hello():
-    form = HelloForm(request.form)
+#esta funcion se ejecuta cuando en index.php en el action="/resultadoIA" llamando a la funcion
+@app.route('/resultadoIA', methods=['POST'])
+def resultadoIA():
+    form = evaluarForm(request.form)
     if request.method == 'POST' and form.validate():
-        name = request.form['sayhello']
-        return render_template('hello.html', name=name)
-    return render_template('second_app.html', form=form)
+        texto = request.form['evaluarText']
+        y=1
+        proba=1
+        #y, proba = classify(texto)
+        return render_template('resultado.html',
+                                content=texto,
+                                prediction=y,
+                                probability=round(proba*100, 2))
+    return render_template('index.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
